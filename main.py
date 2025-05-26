1c33e6b2fa8bdce53b3dc878081a9bf97b9e4670
gmw593 <gmw@gmail.com>
2025-05-25 21:45:13 -0600

    testing streamlit run

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from Ai import find_keywords
 st.title('Resume tweaker')
 
 if st.file_uploader('gimme a Resume'):
-    st.text('hello world')
+    st.text_input('input the job description')
 else:
     pass
